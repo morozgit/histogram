f41365eee24bceb54e82dd67b4a249a8949297da
morozgit <69896562+morozgit@users.noreply.github.com>
2024-07-16 22:34:53 +0300

    add buttuns

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/apphistogram_qmltyperegistrations.cpp
@@ -0,0 +1,22 @@
+/****************************************************************************
+** Generated QML type registration code
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <QtQml/qqml.h>
+#include <QtQml/qqmlmoduleregistration.h>
+
+
+
+#if !defined(QT_STATIC)
+#define Q_QMLTYPE_EXPORT Q_DECL_EXPORT
+#else
+#define Q_QMLTYPE_EXPORT
+#endif
+Q_QMLTYPE_EXPORT void qml_register_types_histogram()
+{
+    qmlRegisterModule("histogram", 1, 0);
+}
+
+static const QQmlModuleRegistration histogramRegistration("histogram", qml_register_types_histogram);
